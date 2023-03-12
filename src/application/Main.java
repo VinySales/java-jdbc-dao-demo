f5c3a9cf04353e1e6ed30bbdbb5ce012960a87d6
Vinicius Sales Rebouças <vinysalesreboucas@hotmail.com>
2023-03-12 02:11:45 -0300

    add method findAll

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,38 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
-		SellerDao obj = DaoFactory.createSellerDao();
-		Seller seller = obj.findById(2);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("====== TEST 1 - SELLER - FIND BY ID ======");
-		System.out.println(seller);
+		Seller seller1 = sellerDao.findById(2);
+		System.out.println(seller1);
 		
+		System.out.println();
+		
+		System.out.println("====== TEST 2 - SELLER - FIND BY DEPARTMENT ======");
+		List<Seller> seller2 = sellerDao.findByDepartment(new Department(2, "Eletronics"));
+		for (Seller seller : seller2) {
+			System.out.println(seller);
+		}
+		
+		System.out.println();
+		
+		System.out.println("====== TEST 3 - SELLER - FIND ALL ======");
+		List<Seller> seller3 = sellerDao.findAll();
+		for (Seller seller : seller3) {
+			System.out.println(seller);
+		}
+		
+		System.out.println();
 	}
 
 }
